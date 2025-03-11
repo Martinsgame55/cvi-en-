9879b3b345749127179b9d8524ff9a535027e9d1
unknown <martin.hoffmann@infis.cz>
2025-03-11 09:24:27 +0100

    text

---
+++ main.py
@@ -0,0 +1,17 @@
+#Martin Hoffmann
+barva = input("Zadejte vaší oblíbenou barvu")
+#input načte od uživatele zadanou věc do proměné
+číslo = int(input("zadejte své oblíbené číslo"))
+#přetypování na číslo
+if barva == "modrá":
+    print("barva není hezká")
+# vypíše ti to tu větu pokud splní podmínku
+elif barva == "modrá" and barva == "černá":
+    print("barva modrá je hezčí než barva černá")
+# vypíse to větu pokud budesplněna podmínka
+else:
+    print("nemáš vkus")
+    # vypíše to větu když nesplníš předchozí podmínku
+    #git příkaz 1 = git add *
+    #git příkaz 2 = git commit -m "text"
+    #git příkaz 3 = git push
